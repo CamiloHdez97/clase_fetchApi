2982845b43d0f28bbf736ca120398a6c4efa253a
CamiloHdez97 <cahdpk@gmail.com>
2023-05-03 23:08:32 -0500

    casa adelanto

--- component/NavBar.js
+++ component/NavBar.js
@@ -10,7 +10,7 @@ render(){
     this.innerHTML = /* HTML */ `
     
     <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #e3f2fd;">
-  <a class="navbar-brand" href="#"><img src="img/ventas_logo.png" width="60px"></a>
+  <a class="navbar-brand" href="index.html"><img src="img/ventas_logo.png" width="60px"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -18,10 +18,10 @@ render(){
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Clientes</span></a>
+        <a class="nav-link" href="clientes.html">Clientes</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Ventas</a>
+        <a class="nav-link" href="ventas.html">Ventas</a>
       </li>
     </ul>
   </div>
--- service/service-customers.js
+++ service/service-customers.js
@@ -2,13 +2,59 @@ import "../component/NavBar.js";
 import Customers from "../js/customers.js";
 
 const URL_API = "https://64524861bce0b0a0f741cb69.mockapi.io/";
+const regCli = document.querySelector('#regCli');
+const listCli = document.querySelector('#listCli');
+const SearchCli = document.querySelector('#SearchCli');
+
+const getCustomers = async () => {
+
+    try{
+        const respuesta = await fetch(`${URL_API}/Users`);
+        //Respuesta correcta
+        if(respuesta.status === 200){
+            const datos = await respuesta.json();
+            viewDataHtml(datos);
+        } else if(respuesta.status === 401){
+            console.warn('La url no es correcta');
+        } else if(respuesta.status === 404){
+            console.warn('El cliente que buscas no existe');
+        }else {
+            console.warn('Se presento un eror en la petición');
+        }
+    }catch(error){
+        console.error(error);
+    }
+
+}
+
+listCli.addEventListener("click", getCustomers);
 
 
+function viewDataHtml(dataUsers){
+    
+    const divCards = document.querySelector('#cardsUsers');
+    divCards.innerHTML = '';
 
-function getCustomers () {
+    dataUsers.forEach(user => {
+        
+        const cardUser = document.createElement('div');
+        cardUser.innerHTML = /* html */ `
+            <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+                <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Info card title</h5>
+                        <p class="card-text">${user.name} ${user.lastname}</p>
+                        <p class="card-text">${user.email}</p>
+                    </div>
+            </div>
 
-    fetch()
-    .then()
-    .then()
-    .catch()
+            `;
+           
+            divCards.appendChild(cardUser);
+            console.log(dataUsers);
+
+});
+    
+
+
 }
